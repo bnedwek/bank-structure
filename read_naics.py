efad0b62c42eba713fd3b40ad9aad842055a73ff
Brandon Nedwek <bnedwek@gmail.com>
2016-08-20 19:43:16 -0400

    Read NAICS codes and add to Neo4j db

---
+++ read_naics.py
@@ -0,0 +1,31 @@
+## Read NAICS code file ##
+import pandas as pd
+from neo4j.v1 import GraphDatabase, basic_auth
+
+driver = GraphDatabase.driver("bolt://localhost", auth=basic_auth("neo4j", "subaru02"))
+session = driver.session()
+
+naics_path = 'data/naics07.txt'
+naics_file = pd.read_fwf(naics_path, colspecs=[(8,14), (16,200)], names=['code', 'desc'])
+
+for i, row in naics_file.iterrows():
+	code = row.code
+	desc = row.desc
+
+	if len(code) > 2:
+		parent = code[:-1]
+	else:
+		parent = None
+
+	if desc.startswith('"') and desc.endswith('"'):
+		desc = desc[1:-1]
+
+	session.run("CREATE (a:NAICS {code: {code}, description: {desc}})", {"code": code, "desc": desc})
+
+	if parent:
+		session.run("""
+				MATCH (a:NAICS), (b:NAICS)
+				WHERE a.code = {code} AND b.code = {parent}
+				CREATE (a)-[r:PARENT]->(b)
+				RETURN r
+			""", {"code": code, "parent": parent})
